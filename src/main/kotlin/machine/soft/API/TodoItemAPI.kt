5c8ee9c150c0092488888e6b898fe48d1d368a0a
kaiklemmer <kai.k770@lice.de>
2022-09-02 14:12:43 +0200

    add more CRUD methods

--- src/main/kotlin/machine/soft/API/TodoItemAPI.kt
+++ src/main/kotlin/machine/soft/API/TodoItemAPI.kt
@@ -2,7 +2,11 @@ package machine.soft.API
 
 import machine.soft.dto.TodoItemDto
 import machine.soft.service.TodoItemService
+import java.util.UUID
+import javax.ws.rs.DELETE
+import javax.ws.rs.GET
 import javax.ws.rs.POST
+import javax.ws.rs.PUT
 import javax.ws.rs.Path
 
 @Path("/todo")
@@ -10,4 +14,13 @@ class TodoItemAPI( val s: TodoItemService) {
 
     @POST
     fun create(dto: TodoItemDto) = s.create(dto)
+
+    @DELETE
+    fun delete(dto: TodoItemDto) = s.delete(dto)
+
+    @PUT
+    fun update(dto: TodoItemDto, id: UUID) = s.update(dto,id)
+
+    @GET
+    fun get(id: UUID) = s.get(id)
 }
--- src/main/kotlin/machine/soft/service/TodoItemService.kt
+++ src/main/kotlin/machine/soft/service/TodoItemService.kt
@@ -3,7 +3,17 @@ package machine.soft.service
 import io.smallrye.mutiny.Uni
 import machine.soft.database.TodoItemRepository
 import machine.soft.dto.TodoItemDto
+import machine.soft.entity.TodoItem
+import java.util.*
 
 class TodoItemService(val r: TodoItemRepository){
     fun create(dto: TodoItemDto): Uni<TodoItemDto> = r.create(dto).onItem().ifNotNull().transform(::TodoItemDto)
+    fun delete(dto: TodoItemDto): Uni<Boolean> = r.deleteById(TodoItem(dto)).onItem().ifNotNull().transform { if(it) it else false }
+
+    fun update(dto: TodoItemDto, id: UUID) {
+        TODO("implement")
+    }
+    fun get(id: UUID) {
+        TODO("implement")
+    }
 }
