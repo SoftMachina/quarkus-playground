2b4176ddb9380ba848536ac8fe9fdec18ff64fb5
kaiklemmer <kai.k770@lice.de>
2022-09-02 13:55:02 +0200

    refactor code

--- src/main/kotlin/machine/soft/database/TodoItemRepository.kt
+++ src/main/kotlin/machine/soft/database/TodoItemRepository.kt
@@ -1,7 +1,6 @@
 package machine.soft.database
 
 import io.quarkus.hibernate.reactive.panache.PanacheRepositoryBase
-import io.quarkus.hibernate.reactive.panache.common.runtime.ReactiveTransactional
 import io.smallrye.mutiny.Uni
 import machine.soft.dto.TodoItemDto
 import machine.soft.entity.TodoItem
@@ -11,6 +10,5 @@ import javax.enterprise.context.Dependent
 @Dependent
 class TodoItemRepository: PanacheRepositoryBase<UUID, TodoItem> {
 
-    @ReactiveTransactional
     fun create(todoItemDto: TodoItemDto): Uni<TodoItem> = TodoItem(todoItemDto).persistAndFlush()
 }
