c08c7f32cf24095e3dafd84be185ab2a5e19aebc
kaiklemmer <kai.k770@lice.de>
2022-09-06 13:27:31 +0200

    provide default values for noArg construction

--- src/main/kotlin/machine/soft/dto/TodoItemDto.kt
+++ src/main/kotlin/machine/soft/dto/TodoItemDto.kt
@@ -2,7 +2,7 @@ package machine.soft.dto
 
 import machine.soft.entity.TodoItem
 
-data class TodoItemDto(var name: String, var description: String) : BaseDto() {
+data class TodoItemDto(var name: String, var description: String?) : BaseDto() {
     constructor(entity: TodoItem) : this(
         name = entity.name,
         description = entity.description
--- src/main/kotlin/machine/soft/entity/TodoItem.kt
+++ src/main/kotlin/machine/soft/entity/TodoItem.kt
@@ -5,8 +5,8 @@ import javax.persistence.Entity
 
 @Entity
 class TodoItem(
-    var name: String,
-    var description: String) : BaseEntity() {
+    var name: String = "",
+    var description: String? = "") : BaseEntity() {
 
     constructor(dto: TodoItemDto) : this(name = dto.name, description = dto.description) {
     }
