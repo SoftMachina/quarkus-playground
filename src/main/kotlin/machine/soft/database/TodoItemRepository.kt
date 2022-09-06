969b305688411ece36dde990778cb0d2aa12cced
kaiklemmer <kai.k770@lice.de>
2022-09-06 13:15:37 +0200

    refacrtor code

--- src/main/kotlin/machine/soft/database/TodoItemRepository.kt
+++ src/main/kotlin/machine/soft/database/TodoItemRepository.kt
@@ -12,7 +12,4 @@ class TodoItemRepository: PanacheRepositoryBase<TodoItem, UUID> {
 
     fun create(dto: TodoItemDto): Uni<TodoItem> = TodoItem(dto).persistAndFlush()
 
-    fun update(id: UUID, dto: TodoItemDto): Uni<TodoItem> = findById(id).onItem().transform {
-        it.apply { persistAndFlush(update(dto.name,dto.description)) }
-    }
 }
