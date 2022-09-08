ef741d7fc109a47ebf4133867d825f276fc739e5
kaiklemmer <kai.k770@lice.de>
2022-09-08 15:05:10 +0200

    refactor code

--- src/main/kotlin/machine/soft/database/TodoItemRepository.kt
+++ src/main/kotlin/machine/soft/database/TodoItemRepository.kt
@@ -5,7 +5,7 @@ import io.quarkus.hibernate.reactive.panache.common.runtime.ReactiveTransactiona
 import io.smallrye.mutiny.Uni
 import machine.soft.dto.TodoItemDto
 import machine.soft.entity.TodoItem
-import java.util.UUID
+import java.util.*
 import javax.enterprise.context.Dependent
 
 @Dependent
@@ -14,8 +14,8 @@ class TodoItemRepository: PanacheRepositoryBase<TodoItem, UUID> {
     fun create(dto: TodoItemDto): Uni<TodoItem> = TodoItem(dto).persistAndFlush()
 
     @ReactiveTransactional
-    fun update(id: UUID, entity: TodoItem): Uni<TodoItemDto> = findById(id).onItem().ifNotNull().transformToUni {
-        it -> it.apply { update(entity) }.persist()
+    fun update(id: UUID, entity: TodoItem): Uni<TodoItem> = findById(id).onItem().ifNotNull().transformToUni { it ->
+        it.apply { update(entity) }.persist()
     }
 
 }
--- src/main/kotlin/machine/soft/service/TodoItemService.kt
+++ src/main/kotlin/machine/soft/service/TodoItemService.kt
@@ -2,20 +2,20 @@ package machine.soft.service
 
 import io.quarkus.hibernate.reactive.panache.common.runtime.ReactiveTransactional
 import io.smallrye.mutiny.Uni
-import machine.soft.database.TodoItemRepository
-import machine.soft.dto.TodoItemDto
-import machine.soft.entity.TodoItem
+import machine.soft.database.TodoCategoryRepository
+import machine.soft.dto.TodoCategoryDto
+import machine.soft.entity.TodoCategory
 import java.util.*
 import javax.enterprise.context.ApplicationScoped
 
 @ApplicationScoped
-class TodoItemService(val r: TodoItemRepository){
-    fun create(dto: TodoItemDto): Uni<TodoItemDto> = r.create(dto).onItem().ifNotNull().transform(::TodoItemDto)
+class TodoItemService(val r: TodoCategoryRepository) {
+    fun create(dto: TodoCategoryDto): Uni<TodoCategoryDto> = r.create(dto).onItem().ifNotNull().transform(::TodoCategoryDto)
 
     @ReactiveTransactional
     fun delete(id: UUID): Uni<Boolean> = r.deleteById(id).onItem().ifNotNull().transform { it }
 
-    fun getById(id: UUID): Uni<TodoItemDto> = r.findById(id).onItem().ifNotNull().transform(::TodoItemDto)
+    fun getById(id: UUID): Uni<TodoCategoryDto> = r.findById(id).onItem().ifNotNull().transform(::TodoCategoryDto)
 
-    fun update(id: UUID,dto: TodoItemDto) = r.update(id, TodoItem(dto))
+    fun update(id: UUID, dto: TodoCategoryDto) = r.update(id, TodoCategory(dto)).onItem().ifNotNull().transform(::TodoCategoryDto)
 }
